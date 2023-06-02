ac1be1db76609f67fdbfe73fdecd30b17cb66085
Sellsmart <132261415+Sellsmart@users.noreply.github.com>
2023-06-02 17:57:05 +0200

    Add files via upload

--- script1.js
+++ script1.js
@@ -6,17 +6,20 @@ const buttontrigger = document.getElementById('caller');
 
 check();
 window.addEventListener('scroll', function() {
-  const contentHeight = content.offsetHeight;
-  const scrollPosition = window.scrollY;
-  const windowHeight = window.innerHeight;
-  const totalHeight = contentHeight - windowHeight;
-  const scrollPercentage = Math.min(scrollPosition / totalHeight, 1) * 100; // calculate scroll percentage
-    console.log(scrollPercentage);
-  if (scrollPercentage >= 8 && scrollPercentage <= 31) { // show fixed div between 30% and 70% scroll position
-    fixedDiv.classList.add('unvisible');
-  } else {
-    fixedDiv.classList.remove('unvisible');
+  if(window.innerWidth < 500){
+    const contentHeight = content.offsetHeight;
+    const scrollPosition = window.scrollY;
+    const windowHeight = window.innerHeight;
+    const totalHeight = contentHeight - windowHeight;
+    const scrollPercentage = Math.min(scrollPosition / totalHeight, 1) * 100; // calculate scroll percentage
+      console.log(scrollPercentage);
+    if (scrollPercentage >= 8 && scrollPercentage <= 31) { // show fixed div between 30% and 70% scroll position
+      fixedDiv.classList.add('unvisible');
+    } else {
+      fixedDiv.classList.remove('unvisible');
+    }
   }
+  
 });
 function check(){
   console.log("Client width: " + window.innerWidth)
